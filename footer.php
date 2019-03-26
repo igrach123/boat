ad6da31d27c7c7e501c6f7c8ddc50dd6ded19b8c
Igor <brajkovich.igor@gmail.com>
2019-03-26 17:53:14 +0100

    started work- added favicon and changed gulp

---
+++ footer.php
@@ -0,0 +1,162 @@
+<footer id="footer" class="clearfix">
+<div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                <h2 class="text-center">Kontaktirajte nas</h2>
+                    <h4 class="text-center ">Pošaljite nam upit!</h4>
+                    
+                    <form role="form" method="post" id="reused_form" >
+                        <div class="form-group">
+                            <label for="name"> Name:</label>
+                            <input type="text" class="form-control" id="name" name="name" required maxlength="50">
+                        </div>
+                        <div class="form-group">
+                            <label for="email"> Email:</label>
+                            <input type="email" class="form-control" id="email" name="email" required maxlength="50">
+                        </div>
+                        <div class="form-group">
+                            <label for="name"> Message:</label>
+                            <textarea class="form-control" type="textarea" name="message" id="message" placeholder="Your Message Here" maxlength="6000" rows="7"></textarea>
+                        </div>
+                        <div class="row" style="margin-bottom:30px;">
+                            <div class="col-sm-5">
+                                <img src="captcha.php" id="captcha_image"/>
+                                <br/>
+                                <a id="captcha_reload" href="#">reload</a> 
+                            </div>
+                            <div class="col-sm-6">
+                                <label for="email">Enter the code from the image here:</label>
+                                <input type="text" class="form-control" required id="captcha" name="captcha" >
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-lg btn-success pull-right" id="btnContactUs">Post It! &rarr;</button>
+                    </form>
+                    <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Sent your message successfully!</h3> </div>
+                    <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
+                </div>
+            </div>
+        </div>
+
+
+            <div class="container">
+              
+
+
+             
+             <div class="row">
+             
+                 <div class="col-xs-12 col-sm-8 col-sm-offset-2  col-md-offset-3 col-md-6" id="contact-container">
+                     
+                     
+                 </div>
+             </div>
+                
+
+                <div class="row footer-top clearfix">
+                  
+                    <div class="col-xs-12 col-sm-6 col-md-3 footer-content ">
+                                    <h4 class=""> <span><i class="fa fa-caret-right"></i></span> KONTAKT</h4>
+                                    
+                                    <ul> 
+                                    <li><h5>R.T.O. - Boris Ivančić </h5></li>   
+                                    <li><span><i class="fa fa-phone" aria-hidden="true"></i></span> <a href="tel:+385 (0)91 512 36 46"> +385 (0)91 512 36 46</a></li>
+                                     <li><span><i class="fa fa-envelope-o" aria-hidden="true"></i></span>  
+                                        <script TYPE="text/javascript">
+                                        emailE=('bivancic@' + 'yahoo.com')
+                                        document.write('<a href="mailto:' + emailE + '">' + emailE + '</a>')            
+                                            </script>
+
+                                            <noscript>
+                                                     <em>Email address protected by JavaScript.<BR>
+                                                         Please enable JavaScript to contact me.</em>
+                                            </noscript>
+                                     </li>
+                                  <li>Zoranićeva 17, Pula</li>
+                                  <li> OIB: 46116579192  </li>
+                                  <li>MB Obrta: 90645405 </li>   
+                                 </ul>                                 
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3  footer-content ">
+                        <h4 class=""> <span><i class="fa fa-caret-right"></i></span>  KORISNI LINKOVI</h4>
+                       <ul>
+                           <li><a href="http://www.vows.at/" target="_blank">www.vows.at</a></li>
+                           <li><a href="https://www.continentseven.com/" target="_blank">Continent seven</a></li>
+                           <li><a href="https://www.pwaworldtour.com/index.php?id=920" target="_blank">PWA Worldtour</a></li>
+                           <li><a href="https://www.windguru.cz/21" target="_blank">windguru.cz</a></li>
+                           <li><a href="http://www.hjs.hr/" target="_blank">HJS</a></li>
+                           <li><a href="https://www.getmyboat.com/" target="_blank">GetMyBoat</a></li>
+                           
+                       </ul>   
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 footer-content visible-xs hidden-sm visible-md visible-lg   ">
+                            <h4 class=""> <span><i class="fa fa-caret-right"></i></span>  VRIJEME</h4>  
+                                <ul>
+                                    <li><a href="http://meteo.hr/index.php" target="_blank">meteo.hr</a></li>
+                                    <li><a href="http://www.arso.gov.si/vreme/napovedi%20in%20podatki/index.html" target="_blank">www.arsov.gov.si</a></li>
+                                    <li><a href="https://www.windguru.cz/21" target="_blank">windguru.cz</a></li>
+
+                                        
+                                  
+
+                                    
+                                </ul>
+                           <!--  footer weather modal-->       
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 footer-content visible-xs hidden-sm  visible-md visible-lg ">
+                            <h4 class=""> <span><i class="fa fa-caret-right"></i></span> SMJEŠTAJ</h4>  
+                                <ul>
+                                    <li><a href="https://www.arenaturist.com/" target="_blank">arenaturist.com</a></li>
+                                    <li><a href="http://www.tzom.hr/" target="_blank">tzcom.hr</a></li>
+                                    <li><a href="http://medulin.hr/" target="_blank">medulin.hr</a></li>
+
+                                </ul>
+                                 
+                    </div>
+                </div> <!-- end of roow -->
+        
+
+                <div class="row footer-top2 visible-sm hidden-md hidden-lg">
+                    <div class="col-xs-12 col-sm-6 col-md-3 footer-content ">
+                            <h4 class=""> <span><i class="fa fa-caret-right"></i></span>  VRIJEME</h4>  
+                                <ul>
+                                    <li><a href="http://meteo.hr/index.php" target="_blank">meteo.hr</a></li>
+                                    <li><a href="http://www.arso.gov.si/vreme/napovedi%20in%20podatki/index.html" target="_blank">www.arsov.gov.si</a></li>
+                                    <li><a href="https://www.windguru.cz/21" target="_blank">windguru.cz</a></li>
+
+                                        
+                                  
+
+                                    
+                                </ul>
+                           <!--  footer weather modal-->       
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3 footer-content ">
+                            <h4 class=""> <span><i class="fa fa-caret-right"></i></span> SMJEŠTAJ</h4>  
+                                <ul>
+                                    <li><a href="https://www.arenaturist.com/" target="_blank">arenaturist.com</a></li>
+                                    <li><a href="http://www.tzom.hr/" target="_blank">tzcom.hr</a></li>
+                                    <li><a href="http://medulin.hr/" target="_blank">medulin.hr</a></li>
+
+                                </ul>
+                                 
+                    </div>
+                </div> <!-- footer-top2 -->
+
+                <div class="container-fluid" id="footer-bottom">
+                   
+                    
+                            <div class="footer-bottom-left"><a href="index.php" target="_self"> 
+                                    <img src="img/ws-logo-big-white-font.png" class="img-responsive" alt="ws-logo"></a>
+                            </div>
+                             <div class="footer-bottom-center">
+                                    <p class="text-center"><?php echo "$the_sitetitle"; ?>  &copy;  <span class="hidden-sm"> <?php echo date("Y"); ?> </span> </p>
+                             </div>
+                              <div class="footer-bottom-right">   
+                                 <a href="https://www.cro-webdesign.com/" target="_blank" > <p class="text-left hidden-xs izradio">Cro Web Design</p></a>
+                             </div>
+                                           
+                </div>
+            </div> <!-- end of container -->
+        
+    </footer>
+        
---
+++ php/services.php
@@ -0,0 +1,67 @@
+
+
+<section class="container-fluid" id="services">
+
+		<h2>IZLETI</h2>
+		<div class="container-fluid">
+			<div class="row izleti-row">
+
+			
+				<div class="col-sm-6 col-lg-3 col-5px service-wrap " >
+					<div class="t-1" data-mh="my-group">
+							
+							<div class="price-bg badge"> 350 kn</div>	
+							<h3 class="title">POLUDNEVNI IZLET</h3>
+							<div class="overlay"></div>
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt dolor voluptate maiores deserunt provident quaerat explicabo eos odit ab iusto autem sunt voluptas numquam, dolore tempore officiis? Nihil, explicabo esse.</p>
+							<div class="pmd-card-actions">
+					    		<a href="#services-windsurf" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-windsurf">Više..</a>
+					        	<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" >kontakt</a>
+					    	</div>
+					
+					
+					</div>
+				</div>
+				<div class="col-sm-6 col-lg-3 col-5px service-wrap" >
+					<div class="t-2" data-mh="my-group">
+						<div class="price-bg badge"> 450 kn</div>	
+						<h3 class="title">CJELODNEVNI IZLET</h3>
+						<div class="overlay"></div>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, ipsa? Sequi, natus! Animi optio culpa, inventore adipisci numquam, veniam facere vel magnam ea delectus incidunt aliquid sunt facilis ex a?</p>
+						<!-- Card action -->
+						<div class="pmd-card-actions">
+							<a href="#services-kayaking" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-kayaking">Više..</a>
+							<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-lg-3 col-5px service-wrap" >
+					<div class="t-3" data-mh="my-group">
+						<div class="price-bg badge"> 350 kn</div>	
+						<h3 class="title">URBANA GRADSKA TURA - PULA</h3>
+						<div class="overlay"></div>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quam ipsam, ut necessitatibus quae amet sunt iste assumenda sed accusantium consequuntur velit ullam minus rerum culpa provident ad iure numquam?</p>
+							<!-- Card action -->
+						<div class="pmd-card-actions">
+					    	<a href="#services-bike" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-bike">Više..</a>
+					    	<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
+					    </div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-lg-3 col-5px service-wrap" >
+					<div class="t-4" data-mh="my-group">
+						<div class="price-bg badge"> 350 kn</div>	
+						<h3 class="title">ENDURO TOURS VALTURA BUDAVA KAMENJAK</h3>
+						<div class="overlay"></div>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur magnam architecto reprehenderit, odit consectetur sequi quae, molestias tempore fugiat illum? Odio consectetur harum consequatur. Tempore nesciunt ratione culpa modi.</p>
+						 <!-- Card action -->
+						 <div class="pmd-card-actions">
+					        <a href="#services-sup" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-sup">Više..</a>
+					       <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
+					    </div>
+					</div>
+				</div>
+			
+			</div>
+		</div>
+</section>
