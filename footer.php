2e67bc2835664eea7483e5fc22631be55ce6c68e
Igor <brajkovich.igor@gmail.com>
2019-04-01 19:14:36 +0200

    worked on maps nad sizes

--- footer.php
+++ footer.php
@@ -3,8 +3,9 @@
             <div class="row">
 
                     <div class="col-md-4">
+                    <h4 class="text-center ">Find Us</h4>
                         <div class="google-maps">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38094.689198160864!2d13.958886755066738!3d44.79294009379744!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9544cb64a3fa26f9!2sRent+a+Bike+Centar!5e0!3m2!1sen!2shr!4v1522863383452" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38094.689198160864!2d13.958886755066738!3d44.79294009379744!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9544cb64a3fa26f9!2sRent+a+Bike+Centar!5e0!3m2!1sen!2shr!4v1522863383452" width="100%" height="600" frameborder="0" style="border:0" zoom="100" allowfullscreen></iframe>
                         </div>
                     </div>
 
@@ -49,10 +50,10 @@
                                     <li>Parking Vladibora - Rovinj North</li> 
                                     <hr>
                                     <li><h5><span class="fa fa-phone"></span> TEL /  <span class="fa fa-whatsapp"></span> WHATSAPP</h5> </li>
-                                    <li class="text-muted"> <a href="tel:+385 (0)98441140"> +385 (0)98 441 140</a></li>
+                                    <li> <a href="tel:+385 (0)98441140"> +385 (0)98 441 140</a></li>
                                     <hr>
                                     <li><h5><span class="fa fa-envelope-o"></span> EMAIL</h5></li>
-                                     <li>
+                                     <li class="text-muted">
                                         <script TYPE="text/javascript">
                                         emailE=('boatexcursionstonka@' + 'gmail.com')
                                         document.write('<a href="mailto:' + emailE + '">' + emailE + '</a>')            
--- hr/footer.php
+++ hr/footer.php
@@ -3,6 +3,7 @@
             <div class="row">
 
                     <div class="col-md-4">
+                    <h4 class="text-center ">Pronađite Nas</h4>
                         <div class="google-maps">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38094.689198160864!2d13.958886755066738!3d44.79294009379744!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9544cb64a3fa26f9!2sRent+a+Bike+Centar!5e0!3m2!1sen!2shr!4v1522863383452" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
                         </div>
