99dc476965d18c5948b5944474ac4eca36484395
Igor <brajkovich.igor@gmail.com>
2019-04-01 18:45:09 +0200

    resized bottom footer padgins and sizes

--- footer.php
+++ footer.php
@@ -9,17 +9,17 @@
                     </div>
 
                     <div class="col-md-4">
-                            <h4 class="text-center ">Pošaljite nam upit!</h4>
+                            <h4 class="text-center ">Send us an Message!</h4>
                             
                             <form role="form" method="post" id="reused_form" >
                                 <div class="form-group">
-                                    <input placeholder="Ime" type="text" class="form-control" id="name" name="name" required maxlength="50">
+                                    <input placeholder="Name" type="text" class="form-control" id="name" name="name" required maxlength="50">
                                 </div>
                                 <div class="form-group">
                                     <input placeholder="Email" type="email" class="form-control" id="email" name="email" required maxlength="50">
                                 </div>
                                 <div class="form-group">
-                                    <textarea class="form-control" type="textarea" name="message" id="message" placeholder="Poruka ovdje" maxlength="6000" rows="5"></textarea>
+                                    <textarea class="form-control" type="textarea" name="message" id="message" placeholder="Message" maxlength="6000" rows="5"></textarea>
                                 </div>
                                 <div class="row" style="margin-bottom:30px;">
                                     <div class="col-sm-5">
@@ -32,23 +32,23 @@
                                     </div>
                                 </div>
                                 <div class="text-center">
-                                    <button type="submit" class="btn  btn-default" id="btnContactUs">POŠALJI</button>
+                                    <button type="submit" class="btn  btn-default" id="btnContactUs">SEND</button>
                                 </div>
                                 
                             </form>
-                            <div id="success_message" style="width:100%; height:100%; display:none; "> <h3 class="bg-success">Poruka uspješno poslana</h3> </div>
-                            <div id="error_message" style="width:100%; height:100%; display:none; "> <h3 class="bg-danger">Greška</h3>Pkušajte ponovno, ili refrešajte stranicu i formu. </div>
+                            <div id="success_message" style="width:100%; height:100%; display:none; "> <h3 class="bg-success">Your message has been sent! </h3> </div>
+                            <div id="error_message" style="width:100%; height:100%; display:none; "> <h3 class="bg-danger">Error</h3>Try again or refresh the page! </div>
                     </div>
                     <div class="col-md-4 contact-info">
                         <h4 class="text-center">
-                            KONTAKT
+                            Contact
                         </h4>
 
                         <ul class="list-unstyled contact-list text-left"> 
-                                    <li><h5> <span class="fa fa-map-marker"></span>  ADRESA UKRCAJA</h5></li>  
-                                    <li>Parking Vladibora - Rovinj sjever</li> 
+                                    <li><h5> <span class="fa fa-map-marker"></span> BOARDING POINT</h5></li>  
+                                    <li>Parking Vladibora - Rovinj North</li> 
                                     <hr>
-                                    <li><h5><span class="fa fa-phone"></span> TEL & <span class="fa fa-whatsapp"></span> WHATSAPP</h5> </li>
+                                    <li><h5><span class="fa fa-phone"></span> TEL /  <span class="fa fa-whatsapp"></span> WHATSAPP</h5> </li>
                                     <li class="text-muted"> <a href="tel:+385 (0)98441140"> +385 (0)98 441 140</a></li>
                                     <hr>
                                     <li><h5><span class="fa fa-envelope-o"></span> EMAIL</h5></li>
@@ -64,7 +64,7 @@
                                             </noscript>
                                      </li>
                                      <hr>
-                                     <li><h5>PRATITE NAS</h5></li>
+                                     <li><h5>FOLLOW US</h5></li>
                                      <li class="social-media-bottom"> 
                                          <a  class="facebook" href="https://www.facebook.com/boatexcursionsTonka/" target="_blank"></a> 
                                          
--- hr/footer.php
+++ hr/footer.php
@@ -48,7 +48,7 @@
                                     <li><h5> <span class="fa fa-map-marker"></span>  ADRESA UKRCAJA</h5></li>  
                                     <li>Parking Vladibora - Rovinj sjever</li> 
                                     <hr>
-                                    <li><h5><span class="fa fa-phone"></span> TEL & <span class="fa fa-whatsapp"></span> WHATSAPP</h5> </li>
+                                    <li><h5><span class="fa fa-phone"></span> TEL / <span class="fa fa-whatsapp"></span> WHATSAPP</h5> </li>
                                     <li class="text-muted"> <a href="tel:+385 (0)98441140"> +385 (0)98 441 140</a></li>
                                     <hr>
                                     <li><h5><span class="fa fa-envelope-o"></span> EMAIL</h5></li>
